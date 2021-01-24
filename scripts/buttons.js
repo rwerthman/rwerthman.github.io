2cdd69b645c4149402c88a2f1d1337a66b6ec7f8
Robert Werthman <robertwerthman@Roberts-MacBook-Pro.local>
2021-01-24 17:15:00 -0500

    Added information for work, education, and personal projects. I still need a hobbies or interests sections.

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -2,6 +2,9 @@ function showWorkTimeline() {
     document.getElementById("education_timeline").style.display = "none";
     document.getElementById("educationTimelineButton").classList.remove("active");
 
+    document.getElementById("personal_projects").style.display = "none";
+    document.getElementById("personalProjectsButton").classList.remove("active");
+
     document.getElementById("work_timeline").style.display = "block";
     document.getElementById("workTimelineButton").classList.add("active");
 
@@ -11,6 +14,20 @@ function showEducationTimeline() {
     document.getElementById("work_timeline").style.display = "none";
     document.getElementById("workTimelineButton").classList.remove("active");
 
+    document.getElementById("personal_projects").style.display = "none";
+    document.getElementById("personalProjectsButton").classList.remove("active");
+
     document.getElementById("education_timeline").style.display = "block";
     document.getElementById("educationTimelineButton").classList.add("active");
+}
+
+function showPersonalProjects() {
+    document.getElementById("work_timeline").style.display = "none";
+    document.getElementById("workTimelineButton").classList.remove("active");
+
+    document.getElementById("education_timeline").style.display = "none";
+    document.getElementById("educationTimelineButton").classList.remove("active");
+
+    document.getElementById("personal_projects").style.display = "block";
+    document.getElementById("personalProjectsButton").classList.add("active");
 }
